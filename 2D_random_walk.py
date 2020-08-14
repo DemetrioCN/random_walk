98eba5e0f67069521d19837caa4102517d7ee42d
Demetrio <dem9411@gmail.com>
2020-08-13 22:07:05 -0500

    2D simulation added

--- 2D_random_walk.py
+++ 2D_random_walk.py
@@ -1,14 +1,10 @@
 # Random walk in two dimensions
+# DemetrioCN
 
 import random
 import numpy as np
 import pylab 
 
-def plot_walk(x,y):
-    
-    pylab.title('Random Walk simulations')
-    pylab.plot(x,y)
-    pylab.show()
 
 def distance(x,y):
     x_f = x[-1]
@@ -17,30 +13,39 @@ def distance(x,y):
     d = (x_f**2 + y_f**2)**0.5
     return d
 
-def walk_2D(steps):
+def plot_walk(x,y):
+    pylab.title('Random Walk simulation')
+    pylab.plot(x,y)
+    pylab.xlabel('x')
+    pylab.ylabel('y')
+    pylab.savefig('rand_walk_2D''.png', bbox_inches='tight', dpi=600)
+    pylab.show()
 
+
+def walk_2D(steps):
     x = [0]
     y = [0]
     route = [0,0]
 
     for i in range(steps):
-
         walk = random.choice([[0,1],[0,-1],[1,0],[-1,0]])
-#        print('paso',walk)
- 
         sum_vec = np.array(route) + np.array(walk)
         route = sum_vec
-        print(route)
         x.append(route[0])
         y.append(route[1])
 
         #print(route)
     plot_walk(x,y)
 
-    print('Distancia final: ', distance(x,y))
+    print('Final distance: ', distance(x,y))
+    print("\n")  
     
-
+        
 
 if __name__ == '__main__':
+    print("\n")
+    print(10*"-","Random Walk Simulation in 2D",10*"-")
+    print("\n")
+
     steps = 10000
     walk_2D(steps)
