cef884df1ab4c2acd15be09d3b4b11ecead51d8c
Demetrio <dem9411@gmail.com>
2020-08-13 21:03:35 -0500

    1D distances

--- 1D_random_walk.py
+++ 1D_random_walk.py
@@ -1,36 +1,53 @@
-# Random Walk in one dimension
+# Random Walk in One Dimension
+
+# DemetrioCN
+
 import random
 
-
+# Choose the next step and add it to the previous one
 def walk_1D(steps):
 
     count = 0
     for i in range(steps):
         walk = random.choice([(1),(-1)])
         count += walk
-       # print(walk)
+
     return count
 
 
-
+# Compute the distance from 0
 def random_walk_1D(steps, attempts):
     distance_sqrt = 0
 
     for attempt in range(attempts):
         distance = walk_1D(steps)
         distance_sqrt += distance**2
-        #print('Final point: ',distance)    
-        
+
     average_distance = (distance_sqrt/attempts)**0.5
-    print("Average distance: ", round(average_distance,2))
+    return round(average_distance,2)
 
 
+def main(steps_list, attempts):
+    print("\n")
+    print(10*"-","RandomWalk's distances in One Dimension" ,10*"-")
+    print("\n")
 
+    print(f'Number of attempts: ', attempts)
+
+    print("\n")
+
+    for i in steps_list:
+        d = random_walk_1D(i,attempts)
+        print(f'The final distance for {i} steps is {d}')
+
+    print("\n")
+
+
+# Program entry
 if __name__ == '__main__':
-    steps = 25
-    attempts = 1000
+    steps_list = [100,200,300,400,500]
+    attempts = 10000
     
-    random_walk_1D(steps, attempts)
-
+    main(steps_list, attempts)
 
 
